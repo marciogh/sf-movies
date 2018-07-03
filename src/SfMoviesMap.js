06330ccd4add404b0f89200aeb56e5499b87cdc7
Marcio Ghiraldelli <mghiraldelli@atlassian.com>
2018-07-04 09:07:17 +1000

    production ready

--- src/SfMoviesMap.js
+++ src/SfMoviesMap.js
@@ -30,59 +30,35 @@ class SfMoviesMap extends React.Component {
 
     handleOnLoad(map) {
         this.setState({
-            map: map
+            map: map,
+            placesService: new google.maps.places.PlacesService(map.map)
         })
-    }
-
-
-    findPlaceFromQueryAsync(placesService, movie) {
-        return new Promise((resolve, reject) => {
-            placesService.findPlaceFromQuery({
-                query: movie.locations,
-                fields: ['name', 'geometry']
-            }, (results, status) => {
-                if (status === 'OK') {
-                    results.map((o) => {
-                        o.movie = movie.title;
-                        return o;
-                    })
-                    resolve(results);
-                } else {
-                    resolve(null);
-                }
-            });
-        });
     }
 
     async handleChange(event) {
-
-        var markers = [];
         this.setState({
-            markers: markers
+            markers: []
         })
         const s = event.target.value;
         if (s.length < 3) return;
-        let placesService = new google.maps.places.PlacesService(this.state.map.map)
         const movies = await NodeFetch('https://data.sfgov.org/resource/wwmu-gmzc.json?$q=' + encodeURI(s));
         const moviesData = await movies.json();
-        let findPlacesPromises = [];
         moviesData.forEach(movie => {
-            findPlacesPromises.push(this.findPlaceFromQueryAsync(placesService, movie))
+            this.state.placesService.findPlaceFromQuery({
+                query: movie.locations,
+                fields: ['name', 'geometry']
+            }, (results, status) => {
+                if (status === 'OK' && results.length > 0) {
+                    results.map((r) => {
+                        r.movie = movie.title;
+                        return r;
+                    });
+                    this.setState({
+                        markers: this.state.markers.concat(results)
+                    })
+                }
+            });
         })
-        Promise.all(findPlacesPromises, {concurrency: 10}).then((r) => {
-            r.filter((o) => {
-                return o != null;
-            }).map((o) => {
-                markers = markers.concat(o);
-                return o;
-            });
-            console.log(markers);
-            this.setState({
-                markers: markers
-            });
-        }).catch((err) => {
-            console.log('error' + err);
-        });
     }
 
     render() {
@@ -99,12 +75,16 @@ class SfMoviesMap extends React.Component {
                     onGoogleApiLoaded={this.handleOnLoad}
                     yesIWantToUseGoogleMapApiInternals={true}
                 >
-                    {this.state.markers.map(m => {
-                        return <SfMoviesComponent
-                            movie={m.movie}
-                            place={m.name}
-                            lat={m.geometry.location.lat()}
-                            lng={m.geometry.location.lng()} />
+                    {
+                        this.state.markers.map(m => {
+                            return (
+                                <SfMoviesComponent
+                                    movie={m.movie}
+                                    place={m.name}
+                                    lat={m.geometry.location.lat()}
+                                    lng={m.geometry.location.lng()}
+                                />
+                            )
                     })}
                 </GoogleMapReact>
             </div>
